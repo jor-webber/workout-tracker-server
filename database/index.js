14f57ed9527008501d5a6cf5165c831fc24f0e97
jordan webber <webber_jordan_95@hotmail.com>
2022-11-16 08:34:51 -0500

    Please work database

--- database/index.js
+++ database/index.js
@@ -1,7 +1,12 @@
 const Pool = require('pg').Pool;
 require('dotenv').config();
 
-const pool = new Pool();
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  }
+});
 
 module.exports = {
   query: (text, params, callback) => pool.query(text, params, callback)
